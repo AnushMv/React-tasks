8bafbed1e2bb3f08f0a06efa5166a427488c6e20
Anush Movsesyan <AnushMv@users.noreply.github.com>
2022-10-04 01:53:08 +0400

    Add files via upload
    counter

---
+++ App.js
@@ -0,0 +1,45 @@
+import {Component} from 'react';
+import Counter from './components/Counter';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            reset: false,
+            isButtonClickable: false,
+        }
+    }
+    resetCounter = () => {
+        this.setState({
+            reset: !this.state.reset
+        })
+
+    }
+    //if you press 'reset Counter' 2 times counter work again, or you need pres 'reset Counter' and 'start Counter'
+    // startCounter = () => {
+    //     this.setState({
+    //         reset: !this.state.reset,
+    //     })
+    // }
+    setIsButtonClickable = () => {
+            this.setState({
+                isButtonClickable: !this.state.isButtonClickable,
+            })
+        }
+
+  render(){
+    const {reset} = this.state;
+
+    // const {start} = this.state;
+    return(
+        <div>
+            <Counter  reset ={reset} />
+            <button className={'reset'} onClick = {this.resetCounter}>Reset counter</button>
+
+                {/*<button className={'start'} onClick = {this.startCounter}>Start counter</button>*/}
+        </div>
+    )
+  }
+}
+
+export default App;
